14266a9b9d857723de6738d9f6929adcecb7e29a
Patrick Ziajski <patrick.ziajski@gmail.com>
2019-11-03 18:27:19 -0500

    working OCR

--- AzureImageRecognition.py
+++ AzureImageRecognition.py
@@ -3,6 +3,9 @@ import sys
 import requests
 import json
 import time
+from pprint import pprint
+
+import CameraCapture
 
 def GetImageJSON(imageName):
     # Add your Computer Vision subscription key and endpoint to your environment variables.
@@ -14,41 +17,24 @@ def GetImageJSON(imageName):
 
     if 'COMPUTER_VISION_ENDPOINT' in os.environ:
         endpoint = os.environ['COMPUTER_VISION_ENDPOINT']
-    else:
-        print("\nSet the COMPUTER_VISION_ENDPOINT environment variable.\n**Restart your shell or IDE for changes to take effect.**")
-        sys.exit()
 
-    text_recognition_url = endpoint + "training\download (1).jpg"
+    ocr_url = endpoint + "vision/v2.1/ocr"
 
-    # Set image_url to the URL of an image that you want to analyze.
-    image_url = "imageName"
-
-    headers = {'Ocp-Apim-Subscription-Key': subscription_key}
-    data = {'url': image_url}
-    response = requests.post(text_recognition_url, headers=headers, json=data)
+    image_path = imageName
+    # Read the image into a byte array
+    image_data = open(image_path, "rb").read()
+    # Set Content-Type to octet-stream
+    headers = {'Ocp-Apim-Subscription-Key': subscription_key, 'Content-Type': 'application/octet-stream'}
+    params = {'language': 'unk', 'detectOrientation': 'true'}
+    # put the byte array into your post request
+    response = requests.post(ocr_url, headers=headers, params=params, data = image_data)
     response.raise_for_status()
 
-    # Extracting text requires two API calls: One call to submit the
-    # image for processing, the other to retrieve the text found in the image.
-
-    # Holds the URI used to retrieve the recognized text.
-    operation_url = response.headers["Operation-Location"]
-
-    # The recognized text isn't immediately available, so poll to wait for completion.
-    analysis = {}
-    poll = True
-    while (poll):
-        response_final = requests.get(
-            response.headers["Operation-Location"], headers=headers)
-        analysis = response_final.json()
-        print(analysis)
-        time.sleep(1)
-        if ("recognitionResults" in analysis):
-            poll = False
-        if ("status" in analysis and analysis['status'] == 'Failed'):
-            poll = False
-
-        # The 'analysis' object contains various fields that describe the image. The most
-        # relevant caption for the image is obtained from the 'description' property.
-        analysis = response.json()
-        print(analysis)
+    analysis = response.json()
+    try:
+        for i in analysis['regions'][0]['lines']:
+            temp = i['words'][0]['text']
+            if len(temp) >= 3:
+                print(temp)
+    except:
+        pprint(analysis)
--- zRecognition.py
+++ zRecognition.py
@@ -1,7 +1,3 @@
-import playsound
-import pigpio
-import pprint
-
 import CameraCapture
 import AzureImageRecognition
 
