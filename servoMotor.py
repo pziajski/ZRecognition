026f8831a6d099817ace3d5202537be0255e91ff
Patrick Ziajski <patrick.ziajski@gmail.com>
2019-11-24 17:03:02 -0500

    testing

--- servoMotor.py
+++ servoMotor.py
@@ -8,6 +8,7 @@ class MotorControl():
     leftDir = 10
 
     def __init__(self):
+        GPIO.cleanup()
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(12, GPIO.OUT)
         self.p = GPIO.PWM(12,50) #pin, frequency
@@ -17,19 +18,16 @@ class MotorControl():
         self.p.start(self.leftDir)
         time.sleep(3)
         self.p.stop()
-        GPIO.cleanup()
     
     def TurnRight(self):
         self.p.start(self.rightDir)
         time.sleep(3)
         self.p.stop()
-        GPIO.cleanup()
 
     def Reset(self):
         self.p.start(self.middDir)
         time.sleep(3)
         self.p.stop()
-        GPIO.cleanup()
 
     def ChangeSpeed(self, dutyCycle):
         self.p.ChangeDutyCycle(dutyCycle)
