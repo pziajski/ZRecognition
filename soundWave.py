4c30dc8fa6aee4f86dbcdd07422c827afaee55be
Patrick Ziajski <patrick.ziajski@gmail.com>
2019-11-24 13:57:38 -0500

    removed unused module

--- soundWave.py
+++ soundWave.py
@@ -1,6 +1,5 @@
 import numpy as np
 import simpleaudio as sa
-from playsound import playsound
 
 
 def soundWave():
@@ -10,11 +9,8 @@ def soundWave():
 
     time = np.linspace(0, seconds, seconds * fs, False)
     note = np.sin(frequency * time * 2 * np.pi)
-
     audio = note * (2**15 - 1) / np.max(np.abs(note))
-
     audio = audio.astype(np.int16)
-
     play_obj = sa.play_buffer(audio, 1,2,fs)
     play_obj.wait_done()
 
